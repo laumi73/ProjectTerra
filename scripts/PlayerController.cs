be5e8a46025328063fe18c8ac98984059587e610
laumi73 <laumichael73@gmail.com>
2022-12-06 00:52:48 -0500

    Added basic camera control system

--- scripts/PlayerController.cs
+++ scripts/PlayerController.cs
@@ -1,42 +1,77 @@
 using Godot;
 using System;
 
-public class PlayerController: Spatial
+public class PlayerController : Spatial
 {
     public float movementSpeed = 0.5f;
     public float movementTime = 5f;
+    public float rotationAmount = 0.1f;
 
     public Vector3 newPosition;
+    public Basis newRotation;
+
+    public Transform defaultPosition = new Transform() {
+            origin = new Vector3(0f, 30f, 0f),
+            basis = new Basis(Vector3.Left, Mathf.Deg2Rad(45f))
+        };
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        Transform = defaultPosition;
         this.newPosition = Translation;
+        this.newRotation = Transform.basis;
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta) {
+    public override void _Process(float delta)
+    {
         this.handleInput(delta);
+        GD.Print(this.RotationDegrees);
     }
 
-    private void handleInput(float delta) {
-        if (Input.IsActionPressed("camera_move_up"))
+    private void handleInput(float delta)
+    {
+        if (Input.IsActionPressed("player_move_up"))
         {
-            this.newPosition -= (Transform.basis.z * this.movementSpeed);
+            this.newPosition += (Vector3.Up * this.movementSpeed);
         }
-        if (Input.IsActionPressed("camera_move_down"))
+        if (Input.IsActionPressed("player_move_down"))
         {
-            this.newPosition += (Transform.basis.z * this.movementSpeed);
+            this.newPosition += (Vector3.Down * this.movementSpeed);
         }
-        if (Input.IsActionPressed("camera_move_left"))
+        if (Input.IsActionPressed("player_move_forward"))
         {
-            this.newPosition -= (Transform.basis.x * this.movementSpeed);
+            this.newPosition += (Vector3.Forward * this.movementSpeed);
         }
-        if (Input.IsActionPressed("camera_move_right"))
+        if (Input.IsActionPressed("player_move_backward"))
         {
-            this.newPosition += (Transform.basis.x * this.movementSpeed);
+            this.newPosition -= (Vector3.Forward * this.movementSpeed);
+        }
+        if (Input.IsActionPressed("player_move_left"))
+        {
+            this.newPosition += (Vector3.Left * this.movementSpeed);
+        }
+        if (Input.IsActionPressed("player_move_right"))
+        {
+            this.newPosition -= (Vector3.Left * this.movementSpeed);
+        }
+        if (Input.IsActionPressed("player_rotate_left"))
+        {
+            this.newRotation = this.newRotation.Rotated(Vector3.Up, rotationAmount);
+        }
+        if (Input.IsActionPressed("player_rotate_right"))
+        {
+            this.newRotation = this.newRotation.Rotated(Vector3.Up, -rotationAmount);
         }
 
         Translation = Translation.LinearInterpolate(newPosition, delta * this.movementTime);
+        Transform = new Transform
+        {
+            origin = Transform.origin,
+            basis = new Basis(Transform.basis.Quat().Slerp(newRotation.Quat(), delta))
+        };
+        Transform = Transform.Orthonormalized();
+        newRotation = newRotation.Orthonormalized();
     }
 }
