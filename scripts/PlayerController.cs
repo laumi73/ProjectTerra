ec596b8a10891ba7e943da91ce82205f998518fa
laumi73 <laumichael73@gmail.com>
2023-01-15 02:11:09 -0500

    Coomenting

--- scripts/PlayerController.cs
+++ scripts/PlayerController.cs
@@ -60,49 +60,49 @@ public class PlayerController : Spatial
         base._PhysicsProcess(delta);
     }
 
-    // Modify transform upon buttonpress/mouseclick
+    // Modify transform upon keyboard press 
     private void handleInput(float delta)
     {
-        if (Input.IsActionPressed("player_move_up"))
+        if (Input.IsActionPressed("player_move_up")) // Move up
         {
             this.newPosition += (Vector3.Up * this.movementSpeed);
         }
-        if (Input.IsActionPressed("player_move_down"))
+        if (Input.IsActionPressed("player_move_down")) // Move down
         {
             this.newPosition += (Vector3.Down * this.movementSpeed);
         }
-        if (Input.IsActionPressed("player_move_forward"))
+        if (Input.IsActionPressed("player_move_forward")) // Move forward
         {
             this.newPosition -= ((this.Transform.basis.z * new Vector3(1f, 0f, 1f)).Normalized() * this.movementSpeed);
         }
-        if (Input.IsActionPressed("player_move_backward"))
+        if (Input.IsActionPressed("player_move_backward")) // Move backward
         {
             this.newPosition += ((this.Transform.basis.z * new Vector3(1f, 0f, 1f)).Normalized() * this.movementSpeed);
         }
-        if (Input.IsActionPressed("player_move_left"))
+        if (Input.IsActionPressed("player_move_left")) // Move left
         {
             this.newPosition -= (this.Transform.basis.x * this.movementSpeed);
         }
-        if (Input.IsActionPressed("player_move_right"))
+        if (Input.IsActionPressed("player_move_right")) // Mvoe right
         {
             this.newPosition += (this.Transform.basis.x * this.movementSpeed);
         }
-        if (Input.IsActionPressed("player_rotate_left"))
+        if (Input.IsActionPressed("player_rotate_left")) // Rotate left
         {
             this.newRotation = this.newRotation.Rotated(Vector3.Up, rotationAmount);
         }
-        if (Input.IsActionPressed("player_rotate_right"))
+        if (Input.IsActionPressed("player_rotate_right")) // Rotate right
         {
             this.newRotation = this.newRotation.Rotated(Vector3.Up, -rotationAmount);
         }
-        if (Input.IsActionPressed("player_zoom_in"))
+        if (Input.IsActionPressed("player_zoom_in")) // Zoom in
         {
             this.newZoom -= (new Vector3(0f, 0f, this.zoomAmount));
             if (this.newZoom.z < 0) {
                 this.newZoom = Vector3.Zero;
             }
         }
-        if (Input.IsActionPressed("player_zoom_out"))
+        if (Input.IsActionPressed("player_zoom_out")) // Zoom out
         {
             this.newZoom += (new Vector3(0f, 0f, this.zoomAmount));
             if (this.newZoom.z > this.maxZoomAmount) {
@@ -110,6 +110,7 @@ public class PlayerController : Spatial
             }
         }
 
+        // Interpolations
         this.playerCamera.Translation = this.playerCamera.Translation.LinearInterpolate(this.newZoom, delta * zoomTime);
         this.Translation = this.Translation.LinearInterpolate(newPosition, delta * this.movementTime);
         Transform orthonormalizedTransform = this.Transform.Orthonormalized();
